17149249114f18adc6273e8dcfd40a536be10c35
RuiZhang <793951781@qq.com>
2021-05-11 17:40:46 +0900

    LeetCode No.145
    AC Leet Code Question No.145

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q145.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q145.java
@@ -1,4 +1,4 @@
-package per.eicho.demo.leetcode.q1_100;
+package per.eicho.demo.leetcode.q101_200;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,14 +6,14 @@ import java.util.List;
 import per.eicho.demo.leetcode.datastructure.TreeNode;
 
 /**
- * <p>94. Binary Tree Inorder Traversal 的题解代码 </p>
+ * <p>145. Binary Tree Postorder Traversal 的题解代码 </p>
  * 
  * <pre>
- *  Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ *  Given the root of a binary tree, return the postorder traversal of its nodes' values.
  * 
  * Example 1: 
  *    Input: root = [1,null,2,3]
- *    Output: [1,3,2]
+ *    Output: [3,2,1]
  * 
  * Example 2:
  *    Input: root = []
@@ -29,52 +29,52 @@ import per.eicho.demo.leetcode.datastructure.TreeNode;
  * 
  * Example 5:
  *    Input: root = [1,null,2]
- *    Output: [1,2]
+ *    Output: [2,1]
  * 
  *  Constraints:
  *   1. The number of nodes in the tree is in the range [0, 100].
  *   2. -100 <= Node.val <= 100
  * </pre>
- * @see <a href="https://leetcode.com/problems/binary-tree-inorder-traversal/">94. Binary Tree Inorder Traversal</a>
+ * @see <a href="https://leetcode.com/problems/binary-tree-postorder-traversal/">145. Binary Tree Postorder Traversal</a>
  */
-final public class Q94 {
+final public class Q145 {
 
     /**
      * <p>inorder traversal</p>
      * 
      * <pre>
-     *  do inorder traversal for the given binary tree.
+     *  do postorder traversal for the given binary tree.
      * 
-     *  inorder traversal is a kind of depth first traversals,
-     *  which traversal the tree using left, root, right order. 
+     *  postorder traversal is a kind of depth first traversals,
+     *  which traversal the tree using left, right, root order. 
      * </pre>
      * 
      * @param root
      * @return
      * 
      * @link https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
-     */
-    public List<Integer> inorderTraversal(TreeNode root) {
+     */    
+    public List<Integer> postorderTraversal(TreeNode root) {
         final List<Integer> result = new ArrayList<>();
 
         if (root != null) {
-            doInorderTraversal(root, result);
+            dopostorderTraversal(root, result);
         }
 
         return result;
     }
 
-    private void doInorderTraversal(final TreeNode node, final List<Integer> result) {
+    private void dopostorderTraversal(final TreeNode node, final List<Integer> result) {
         //assert node != null;
 
         if (node.left != null) {
-            doInorderTraversal(node.left, result);
+            dopostorderTraversal(node.left, result);
         }
-
-        result.add(node.val);
 
         if (node.right != null) {
-            doInorderTraversal(node.right, result);
-        }
+            dopostorderTraversal(node.right, result);
+        }        
+
+        result.add(node.val);
     }
 }
