3ce9fdf9204d36828528bcf04f244f023a7977b5
RuiZhang <793951781@qq.com>
2021-11-10 15:37:38 +0900

    LeetCode No.96
    AC Leet Code Question No.96

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q1_100/Q96.java
@@ -0,0 +1,30 @@
+package per.eicho.demo.leetcode.q1_100;
+
+/**
+ * <p>96. Unique Binary Search Trees 的题解代码 </p>
+ * 
+ * @see <a href="https://leetcode.com/problems/unique-binary-search-trees/">96. Unique Binary Search Trees</a>
+ */
+public final class Q96 {
+    final int[] f = new int[20]; // 0-19
+    
+    public int numTrees(int n) {
+        f[0] = 1;
+        f[1] = 1;
+
+        return dp(n);
+    }
+
+    public int dp(int n) {
+        if (f[n] != 0) return f[n];
+
+        int result = 0;
+        final int sum = n - 1;
+        for (int i = 0; i < n; i++) {
+            result += dp(i) * dp(sum - i);
+        }
+
+        f[n] = result;
+        return result;
+    }
+}
