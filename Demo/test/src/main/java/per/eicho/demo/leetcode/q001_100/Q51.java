aa1f01021506106f886528bc4851e01521f3941a
RuiZhang <793951781@qq.com>
2021-11-19 08:56:13 +0900

    Rename package
    q1_100 -> q001_100

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q001_100/Q51.java
@@ -0,0 +1,14 @@
+package per.eicho.demo.leetcode.q1_100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final public class Q51 {
+    public List<List<String>> solveNQueens(int n) {
+        final List<List<String>> result = new ArrayList<>();
+
+        
+
+        return result;
+    }
+}
