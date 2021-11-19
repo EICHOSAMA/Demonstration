c9dc02588d59553d5dfb7017e42dab8cc83bd22e
RuiZhang <793951781@qq.com>
2021-11-19 09:46:49 +0900

    LeetCode No.461
    AC Leet Code Question No.461

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q401_500/Q461.java
@@ -0,0 +1,19 @@
+package per.eicho.demo.leetcode.q401_500;
+
+/**
+ * <p>461. Hamming Distance 的题解代码 </p>
+ * 
+ * @see <a href="https://leetcode.com/problems/maximum-average-subarray-i/">461. Hamming Distance</a>
+ */
+public final class Q461 {
+    public int hammingDistance(int x, int y) {
+        int n = x ^ y;
+        
+        int result = 0;
+        while (n > 0) {
+            result += n & 1;
+            n >>= 1;
+        }
+        return result;
+    }
+}
