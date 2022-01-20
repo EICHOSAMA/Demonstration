575e10a879025d711bf42c62198a326289f882ce
RuiZhang <793951781@qq.com>
2022-01-20 17:08:58 +0900

    LeetCode No.1171
    AC Leet Code Question No.1171

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q1101_1200/Q1171.java
@@ -0,0 +1,84 @@
+package per.eicho.demo.leetcode.q1101_1200;
+
+import java.util.HashMap;
+
+import per.eicho.demo.leetcode.datastructure.ListNode;
+
+/**
+ * <p>1171. Remove Zero Sum Consecutive Nodes from Linked List 的题解代码 </p>
+ * 
+ * @see <a href="https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/">1171. Remove Zero Sum Consecutive Nodes from Linked List</a>
+ */
+public final class Q1171 {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        // The given linked list will contain between 1 and 1000 nodes.
+        // Each node in the linked list has -1000 <= node.val <= 1000.
+
+        // Key: Sum , Val: node
+        HashMap<Integer, ListNode> map = new HashMap<>();
+        ListNode vituralHead = new ListNode(0);
+        vituralHead.next = head;
+        map.put(0, vituralHead);
+
+        int sum = 0;
+        
+        ListNode cursor = head;
+        while (cursor != null) {
+            sum += cursor.val;
+            final Integer iSum = sum;
+
+            if (map.containsKey(iSum)) {
+                final ListNode node = map.get(iSum);
+                
+                ListNode delCursor = node.next;
+                node.next = cursor.next;
+
+                int delSum = sum;
+                while (delCursor != cursor) {
+                    delSum += delCursor.val;
+                    map.remove(delSum);
+                    delCursor = delCursor.next;
+                }
+
+            } else {
+                map.put(iSum, cursor);
+            }
+
+            cursor = cursor.next;
+        }
+        return vituralHead.next;
+    }
+
+    public static void main(String[] args) {
+        Q1171 q1171 = new Q1171();
+
+        
+        ListNode head = new ListNode(1);
+        ListNode tail = head;
+        tail.next = new ListNode(3);
+        tail = tail.next;
+        tail.next = new ListNode(2);
+        tail = tail.next;
+        tail.next = new ListNode(-3);
+        tail = tail.next;
+        tail.next = new ListNode(-2);
+        tail = tail.next;
+        tail.next = new ListNode(5);
+        tail = tail.next;
+        tail.next = new ListNode(5);        
+        tail = tail.next;
+        tail.next = new ListNode(-5);
+        tail = tail.next;
+        tail.next = new ListNode(1);
+        tail = tail.next;
+        
+
+        ListNode result = q1171.removeZeroSumSublists(head);
+
+        while (result != null) {
+            System.out.print(result.val);
+            System.out.print(",");
+            result = result.next;
+        }
+    }
+}
