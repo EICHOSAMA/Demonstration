0a523f1ca2fb752ae15f01e3790502564d1bbca9
RuiZhang <793951781@qq.com>
2022-01-25 22:30:46 +0900

    LeetCode No.2068
    AC Leet Code Question No.2068

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q2001_2100/Q2068.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q2001_2100/Q2068.java
@@ -1,6 +1,27 @@
 package per.eicho.demo.leetcode.q2001_2100;
 
-
+/**
+ * <p>2068. Check Whether Two Strings are Almost Equivalent 的题解代码 </p>
+ * 
+ * @see <a href="https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/">
+ *   2068. Check Whether Two Strings are Almost Equivalent</a>
+ */
 public final class Q2068 {
-    
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        // 1. n == word1.length == word2.length
+        // 2. 1 <= n <= 100
+        // 3. word1 and word2 consist only of lowercase English letters.
+        final int[] differences = new int[26];
+
+        for (int i = 0; i < word1.length(); i++) {
+            differences[word1.charAt(i) - 'a']++;
+            differences[word2.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < differences.length; i++) {
+            if (Math.abs(differences[i]) > 3) return false;
+        }
+
+        return true;
+    }
 }
