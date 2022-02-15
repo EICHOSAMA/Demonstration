3a4b774af4f11c3a4f39a850018b16f0184faa8a
RuiZhang <793951781@qq.com>
2022-02-15 23:37:37 +0900

    LeetCode No.836
    AC Leet Code Question No.836

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q801_900/Q836.java
@@ -0,0 +1,21 @@
+package per.eicho.demo.leetcode.q801_900;
+
+/**
+ * <p>836. Rectangle Overlap 的题解代码 </p>
+ * 
+ * @see <a href="https://leetcode.com/problems/rectangle-overlap/">836. Rectangle Overlap</a>
+ */
+public final class Q836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // 1. rect1.length == 4
+        // 2. rect2.length == 4
+        // 3. -10^9 <= rec1[i], rec2[i] <= 10^9
+        // 4. rec1 and rec2 represent a valid rectangle with a non-zero area.        
+        if (rec1[2] <= rec2[0]) return false; // r1.x2 <= r2.x1 
+        if (rec1[3] <= rec2[1]) return false; // r1.y2 <= r2.y1
+
+        if (rec2[2] <= rec1[0]) return false; // r2.x2 <= r1.x1 
+        if (rec2[3] <= rec1[1]) return false; // r2.y2 <= r1.y1
+        return true;
+    }
+}
