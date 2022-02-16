ff1a38345f32105f383fca6ef23f029a8c0a9137
RuiZhang <793951781@qq.com>
2022-02-17 08:58:54 +0900

    Sync LeetCode Q.1025

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q1001_1100/Q1025.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q1001_1100/Q1025.java
@@ -9,12 +9,6 @@ package per.eicho.demo.leetcode.q1001_1100;
 public final class Q1025 {
     public boolean divisorGame(int n) {
         // 1. 1 <= n <= 1000
-        final boolean[] f = new boolean[n + 1];
-        f[1] = false;
-        for (int i = 1; i < n; i++) {
-            
-        }
-
-        return f[n];
+        return n % 2 == 0;   
     }
 }
