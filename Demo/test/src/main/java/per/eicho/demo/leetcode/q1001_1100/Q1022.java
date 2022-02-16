3509e077ba94b84bebf9d57fd6d71abd9af16c30
RuiZhang <793951781@qq.com>
2022-02-17 08:39:42 +0900

    LeetCode No.1022
    AC Leet Code Question No.1022

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q1001_1100/Q1022.java
@@ -0,0 +1,25 @@
+package per.eicho.demo.leetcode.q1001_1100;
+
+import per.eicho.demo.leetcode.datastructure.TreeNode;
+
+public final class Q1022 {
+    public int sumRootToLeaf(TreeNode root) {
+        // 1. The number of nodes in the tree is in the range [1, 1000].
+        // 2. Node.val is 0 or 1.
+        return sum(root, 0);
+    }
+
+    private int sum(TreeNode node, int prefix) {
+        prefix = prefix << 1 + node.val;
+        if (node.left == null && node.right == null) {
+            System.out.println(prefix);
+            return prefix;
+        }
+
+        int sum = 0;
+        if (node.left != null) sum += sum(node.left, prefix);
+        if (node.right != null) sum += sum(node.right, prefix);
+        
+        return sum;
+    }
+}
