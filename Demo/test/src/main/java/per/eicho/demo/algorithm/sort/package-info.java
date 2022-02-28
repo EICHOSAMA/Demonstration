943ff2d8d9626ae2e0b9c8f88bf50f2f09cb792f
RuiZhang <793951781@qq.com>
2022-02-28 15:18:54 +0900

    Append Content to sort package-info file

--- Demo/test/src/main/java/per/eicho/demo/algorithm/sort/package-info.java
+++ Demo/test/src/main/java/per/eicho/demo/algorithm/sort/package-info.java
@@ -1,4 +1,40 @@
 /**
- * 各类排序算法的demo代码将被收录在此package里。
+ * <p>各类排序算法的demo代码将被收录在此package里。</p>
+ * 
+ * <table border="1">
+ *   <tr>
+ *      <td>编号</td>
+ *      <td>排序种类</td>
+ *   </tr>
+ *   <tr>
+ *      <td>1</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.BubbleAscendingSort 冒泡排序}</td>
+ *   </tr>
+ *   <tr>
+ *      <td>2</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.HeapAscendingSort 堆排序}</td>
+ *   </tr>
+ *   <tr>
+ *      <td>3</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.InsertionAscendingSort 插入排序}</td>
+ *   </tr>
+ *   <tr>
+ *      <td>4</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.MergeAscendingSort 归并排序}</td>
+ *   </tr>
+ *   <tr>
+ *      <td>5</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.ParallelMergeAscendingSort 并行归并排序}</td>
+ *   </tr>
+ *   <tr>
+ *      <td>6</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.RadixAscendingSort 基数排序}</td>
+ *   </tr> 
+ *   <tr>
+ *      <td>7</td>
+ *      <td>{@link per.eicho.demo.algorithm.sort.SelectionAscendingSort 选择排序}</td>
+ *   </tr>
+ * </table>
+ * 
  */
 package per.eicho.demo.algorithm.sort;
