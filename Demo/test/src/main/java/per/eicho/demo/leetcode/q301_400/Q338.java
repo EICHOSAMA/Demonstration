c3cafed1c07af91182d0f407a7c0b6e3ae306ab3
RuiZhang <793951781@qq.com>
2022-04-05 20:43:57 +0900

    Optimized

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q301_400/Q338.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q301_400/Q338.java
@@ -7,18 +7,19 @@ package per.eicho.demo.leetcode.q301_400;
  */
 public final class Q338 {
     public int[] countBits(int num) {
+        // 1. 0 <= n <= 10^5
         final int[] result = new int[num + 1];
         int offsset = 1;
-        while (true) {
+        int i = 0;
+        while (i < num) {
+            int l = offsset, r = offsset * 2; // [l, r)
+            if (r > num) r = num + 1;
             // 0 - offset - 1;
             // offset - offset * 2 - 1
             // offset *= 2;
-            for (int i = offsset; i < offsset * 2 ; i++) {
-                if (i > num)
-                    return result;
-                result[i] = result[i - offsset] + 1;
-            }
-            offsset *= 2;
-        }        
+            for (i = l; i < r ; i++) result[i] = result[i - offsset] + 1;
+            offsset <<= 1;
+        }
+        return result;
     }
 }
