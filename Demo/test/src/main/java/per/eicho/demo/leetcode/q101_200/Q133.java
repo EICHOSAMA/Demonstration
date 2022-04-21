d261cdf3be79410d0e763e0fc4df91a2a3e8a8eb
RuiZhang <793951781@qq.com>
2022-04-21 10:16:31 +0900

    Optimized, apply constant optimization

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q133.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q133.java
@@ -13,6 +13,38 @@ import java.util.Queue;
  * @see <a href="https://leetcode.com/problems/clone-graph/">133. Clone Graph</a>
  */
 public final class Q133 {
+    public Node cloneGraph(Node node) {
+        // 1. The number of nodes in the graph is in the range [0, 100].
+        // 2. 1 <= Node.val <= 100
+        // 3. Node.val is unique for each node.
+        // 4. There are no repeated edges and no self-loops in the graph.
+        // 5. The Graph is connected and all nodes can be visited starting from the given node.
+        if (node == null) return null;
+   
+        final Map<Node, Node> map = new HashMap<>();
+        final Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (queue.size() != 0) {
+            final Node processingNode = queue.poll();
+            final Node copiedNode = new Node(processingNode.val, new ArrayList<>(processingNode.neighbors));
+            map.put(processingNode, copiedNode);
+
+            for (Node nd: processingNode.neighbors) {
+                if (map.containsKey(nd)) continue;
+                queue.add(nd);
+            }
+        }
+
+        for (Node copiedNode: map.values()) {
+            final int size = copiedNode.neighbors.size();
+            for (int i = 0; i < size; i++) {
+                copiedNode.neighbors.set(i, map.get(copiedNode.neighbors.get(i)));
+            }
+        }
+
+        return map.get(node);
+    }
 
     @SuppressWarnings("unused")
     private static final class Node {
@@ -30,43 +62,5 @@ public final class Q133 {
             val = _val;
             neighbors = _neighbors;
         }
-    }
-
-    public Node cloneGraph(Node node) {
-        // 1. The number of nodes in the graph is in the range [0, 100].
-        // 2. 1 <= Node.val <= 100
-        // 3. Node.val is unique for each node.
-        // 4. There are no repeated edges and no self-loops in the graph.
-        // 5. The Graph is connected and all nodes can be visited starting from the given node.
-        if (node == null) return null;
-   
-        final Map<Node, Node> map = new HashMap<>();
-        final Queue<Node> queue = new LinkedList<>();
-        queue.add(node);
-
-        while (queue.size() != 0) {
-            final Node processingNode = queue.poll();
-
-            if (map.containsKey(processingNode)) continue;
-
-            final Node copiedNode = new Node(processingNode.val);
-            map.put(processingNode, copiedNode);
-
-            for (int i = 0; i < processingNode.neighbors.size(); i++) {
-                if (map.containsKey(processingNode.neighbors.get(i))) continue;
-                queue.add(processingNode.neighbors.get(i));
-            }
-        }
-
-        for (Map.Entry<Node, Node> entry : map.entrySet()) {
-            final Node originalNode = entry.getKey();
-            final Node copiedNode = entry.getValue();
-
-            for (int i = 0; i < originalNode.neighbors.size(); i++) {
-                copiedNode.neighbors.add(map.get(originalNode.neighbors.get(i)));
-            }
-        }
-
-        return map.get(node);
     }
 }
