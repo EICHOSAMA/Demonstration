417cc40cb305e4b95a37f0720caaeee3f6415474
RuiZhang <793951781@qq.com>
2022-05-13 09:47:16 +0900

    Optimized, Space Complexity O(1) Solution.

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q117.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q101_200/Q117.java
@@ -15,22 +15,34 @@ public final class Q117 {
         // 1. The number of nodes in the tree is in the range [0, 6000].
         // 2. -100 <= Node.val <= 100
         if (root == null) return null;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            final int size = queue.size();
-            for (int i = 1; i < size; i++) {
-                final Node node = queue.poll();
-                addSubNode2Queue(node, queue);
-                node.next = queue.peek();
-            }
-            addSubNode2Queue(queue.poll(), queue);
-        }
-        return root;
-    }
+        
+        Node cursor = root;
+        final Node vituralHead = new Node(-101);
+        Node tail = vituralHead; // vitural
+        while (cursor != null) {
 
-    private void addSubNode2Queue(Node node, Queue<Node> queue) {
-        if (node.left != null) queue.add(node.left);
-        if (node.right != null) queue.add(node.right);
+            while (cursor != null) {
+                if (cursor.left != null) {
+                    tail.next = cursor.left;
+                    tail = tail.next;
+                }
+
+                if (cursor.right != null) {
+                    tail.next = cursor.right;
+                    tail = tail.next;
+                }
+
+                cursor = cursor.next;
+            }
+
+            // move to next layer
+            cursor = vituralHead.next;
+
+            // reset VH .
+            vituralHead.next = null;
+            tail = vituralHead;
+        }
+
+        return root;
     }
 }
