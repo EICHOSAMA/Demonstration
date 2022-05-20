f2f85d59c3c8444b09472ffd3007cb611ae274dd
RuiZhang <793951781@qq.com>
2022-05-20 10:01:44 +0900

    AC

--- Demo/test/src/main/java/per/eicho/demo/leetcode/q001_100/Q63.java
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q001_100/Q63.java
@@ -11,27 +11,22 @@ public final class Q63 {
         // 2. n == obstacleGrid[i].length
         // 3. 1 <= m, n <= 100
         // 4. obstacleGrid[i][j] is 0 or 1.
-        
         final int OBSTACLE = 1;
-        // Fix: for wrong test case. [{1}] -> 1.
         if (obstacleGrid[0][0] == OBSTACLE) return 0;
 
-        final int row = obstacleGrid.length;
-        final int col = obstacleGrid[0].length;
+        final int m = obstacleGrid.length;
+        final int n = obstacleGrid[0].length;
 
-        int[][] f = new int[row][col];
+        final int[][] f = new int[m][n];
         f[0][0] = 1;
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (obstacleGrid[i][j] != OBSTACLE) {
-                    if (i-1 >= 0)
-                        f[i][j] += f[i-1][j];
-                    if (j-1 >= 0)
-                        f[i][j] += f[i][j-1];
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == OBSTACLE) continue;
+                if (i - 1 >= 0) f[i][j] += f[i - 1][j];
+                if (j - 1 >= 0) f[i][j] += f[i][j - 1];
             }
         }
-        return f[row - 1][col - 1];        
+        return f[m - 1][n - 1];        
     }
 }
