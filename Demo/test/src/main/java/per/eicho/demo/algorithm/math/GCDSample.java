54a9cbad936612a65defa8662de61189b8b96c24
RuiZhang <793951781@qq.com>
2022-07-24 02:13:13 +0800

    新增关联的求LCM方法。

--- Demo/test/src/main/java/per/eicho/demo/algorithm/math/GCDSample.java
+++ Demo/test/src/main/java/per/eicho/demo/algorithm/math/GCDSample.java
@@ -22,5 +22,23 @@ public final class GCDSample {
         return num1;
     }
 
+    /**
+     * <p>求最小公倍数(Least Common Multiple)</p>
+     * 
+     * <pre>
+     *  对于给定的两个<b>正整数</b>求其最小公倍数。
+     * </pre>
+     * 
+     * @param num1 正整数
+     * @param num2 正整数
+     * @return 给定正整数的LCM可能超过int类型的范围，返回一个用long存储的正整数。
+     */
+    public static long lcm(int num1, int num2) {
+        if (num1 <= 0 || num2 <= 0) {
+            throw new IllegalArgumentException("给定参数不是正整数");
+        }
+        return ((long)num1 * (long)num2) / (long)gcd(num1, num2);
+    }
+
     private GCDSample() {}
 }
