6311aa3252aad16e8b907f8263a03d75ca413b38
RuiZhang <793951781@qq.com>
2022-10-18 15:54:08 +0800

    新增java.io包的演示package及相关package-info

---
+++ Demo/test/src/main/java/per/eicho/demo/jdk/java/io/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * <p>{@link java.io}包相关的demo代码将被包含在此package里。</p>
+ * <pre>
+ *  io包是初代java1.0版本就存在的一个历史久远的包，主要提供了以下几个功能
+ *    - 系统输入、输出流（System Input / Output Stream）包含下列基础类（foundamental class）及其延伸的子类、包装类等。
+ *      - {@link java.io.Reader}抽象字符输入流
+ *      - {@link java.io.InputStream}抽象字节输入流
+ *      - {@link java.io.Writer}抽象字符输出流
+ *      - {@link java.io.OutputStream}抽象字节输出流
+ *    - 序列化（{@link java.io.Serializable}接口）
+ *    - 文件系统相关（{@link java.io.File}类等）
+ * </pre>
+ * 
+ * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/io/index.html">Java I/O, NIO, and NIO.2 - Oracle</a>
+ * @see <a href="https://docs.oracle.com/javase/tutorial/essential/io/index.html">Basic I/O - Oracle</a>
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/io/package-summary.html">Package java.io - Java8 Oracle doc</a>
+*/
+package per.eicho.demo.jdk.java.io;
