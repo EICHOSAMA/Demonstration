6fc420cf5aae857ace35d66e0896793131b181bd
RuiZhang <793951781@qq.com>
2022-10-28 03:24:24 +0800

    新增数据结构包相关的描述及新增Unclassified子包
    新增未分类数据结构子包及追加一些在数据结构包下package-info里的一些描述。

--- Demo/test/src/main/java/per/eicho/demo/datastructure/package-info.java
+++ Demo/test/src/main/java/per/eicho/demo/datastructure/package-info.java
@@ -1,4 +1,15 @@
 /**
- * 本java包将用于收录各种手撸数据结构，以供参考学习使用。
+ * <p>本java包将用于收录各种手撸数据结构，以供参考学习使用。</p>
+ * <pre>
+ *  datastructure包下主要有几个分类：
+ *    - {@link per.eicho.demo.datastructure.tree Tree}: 用于收录树形数据结构相关的样例类
+ *    - {@link per.eicho.demo.datastructure.graph Graph}: 用于收录图相关数据结构的样例类
+ *    - {@link per.eicho.demo.datastructure.unclassified Unclassified}: 用于收录暂时笔者无法分类的数据结构样例类，未来也许会移动到新包下。
+ * </pre>
+ * 
+ * <h2>注意</h2>
+ * <pre>
+ *   树本质是一种特殊的图就像链表是特殊的树一样，笔者在考虑未来也许会把Tree包整个移动到Graph下。
+ * </pre>
  */
 package per.eicho.demo.datastructure;
---
+++ Demo/test/src/main/java/per/eicho/demo/datastructure/unclassified/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>数据结构 - 未分类的数据结构</p>
+ * 
+ * <pre>
+ *  用于临时收录暂时无法分类的数据结构的大分类package。
+ * </pre>
+ */
+package per.eicho.demo.datastructure.unclassified;
