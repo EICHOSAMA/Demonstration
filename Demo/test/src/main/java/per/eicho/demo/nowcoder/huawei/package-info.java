0816cef33afb7d4a6afaabdf6eb3e93a42874f82
RuiZhang <793951781@qq.com>
2022-11-11 18:37:21 +0800

    新增牛客网相关package-info

---
+++ Demo/test/src/main/java/per/eicho/demo/nowcoder/huawei/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 牛客（now coder）网下华为机考题相关题解将被收录在本package下。
+ * 
+ * 截止目前（2022/11/11）为止华为机考大分类下的题目编号为：如HJ106，HJ3这种。
+ */
+package per.eicho.demo.nowcoder.huawei;
---
+++ Demo/test/src/main/java/per/eicho/demo/nowcoder/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 牛客（now coder）网刷题的相关题解将被收录在本package下。
+ */
+package per.eicho.demo.nowcoder;
