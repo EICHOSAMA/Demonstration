f3e86282907ccb97f6b723607aecee069adf436b
RuiZhang <793951781@qq.com>
2022-11-25 21:27:56 +0800

    AC LeetCode1890

---
+++ Demo/test/src/main/java/per/eicho/demo/leetcode/q1801_1900/Q1890.sql
@@ -0,0 +1,12 @@
+-- 1890. The Latest Login in 2020
+-- https://leetcode.com/problems/the-latest-login-in-2020/description/
+
+SELECT
+    user_id,
+    MAX(time_stamp) as last_stamp
+FROM
+    Logins
+WHERE
+    YEAR(time_stamp) = 2020
+GROUP BY
+    user_id
